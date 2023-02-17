f0d0856e12ebb51ac196bbe9c7a7878cb2bc8459
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-02-17 23:03:44 +0530

    added blur cursor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Navbar from "./Components/Navbar";
+import './App.css'
 
-function App() { 
+function App() {
+  document.body.onpointermove = event => {
+    const blob = document.getElementById('blob');
+    const { clientX, clientY } = event;
+    blob.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, {duration: 2000, fill: "forwards"})
+  }
+
   return (
     <>
+    <div id="blob"></div>
+    <div id="blur"></div>
     <Navbar/>
     <p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p><p>a</p>
     </>
