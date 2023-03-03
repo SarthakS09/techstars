daf8dfe2e771c1fef1ac6dcd07c168bb01648c83
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-03-03 19:34:04 +0530

    added home sections

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-        
+        <div className='events'>
+            <h2>events</h2>
+        </div>
+
+        <div className='speakers'>
+            <h2>events</h2>
+        </div>
+
+        <div className='collaborations'>
+            <h2>events</h2>
+        </div>
     </>
   )
 }
