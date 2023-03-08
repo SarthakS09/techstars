efd414d82f457444f9a39eb12a7c9d508d067af0
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-03-08 11:16:37 +0530

    linked bouncing button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import scrollDown from '../Assets/scroll.svg'
@@ -13,10 +14,12 @@ export default function Home() {
         <h1 className='t1 animate__animated animate__bounceInRight'>🚀 your biggest </h1>
         <h1 className='t2 animate__animated animate__bounceInLeft'>networking 👨🏻‍💻</h1>
         <h1 className='t3 animate__animated animate__bounceInRight'>💸 opportunity.</h1>
+        <Link style={{ width: "100%", display: 'flex', justifyContent: 'center' }} to="events-start" smooth={true} offset={-75} duration={1500}>
         <img src={scrollDown} className='animate__animated animate__bounceInUp scroller'/>
+        </Link>
       </div>
 
-      <h2>mind-blowing events 🤯</h2>
+      <h2 id='events-start'>mind-blowing events 🤯</h2>
       <p className='center'>Sample text demonstrating how cool and diverse our events line-up is</p>
       <div className='events'>
           <div className='shark-tank-box'>
