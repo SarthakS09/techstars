8f66af326d1b00d90be092790f1af52a37ee6228
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-03-09 03:24:20 +0530

    added navbar animations

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import navLogo from '../Assets/logo-white.png';
 import esmtLogo from '../Assets/esmt.svg'
@@ -8,14 +9,19 @@ import '../Styles/Navbar.css'
 
 
 export default function Navbar({ isMobile }) {
+  
   // const [offset, setOffset] = useState(0);
   // useEffect(() => {
-  //     const onScroll = () => setOffset(window.pageYOffset);
-  //     window.removeEventListener('scroll', onScroll);
-  //     window.addEventListener('scroll', onScroll, { passive: true });
-  //     return () => window.removeEventListener('scroll', onScroll);
-  // }, []);
-  // // console.log(offset);
+    //     const onScroll = () => setOffset(window.pageYOffset);
+    //     window.removeEventListener('scroll', onScroll);
+    //     window.addEventListener('scroll', onScroll, { passive: true });
+    //     return () => window.removeEventListener('scroll', onScroll);
+    // }, []);
+    // // console.log(offset);
+
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
       <>
       <div className='nav-fixed'>
@@ -24,12 +30,8 @@ export default function Navbar({ isMobile }) {
           </div>
           <div className='right'>
             <div className='hamburger-wrapper'>
-              <Hamburger size={isMobile ? 18 : 22} color='black'/>
+              <Hamburger className='hb' size={isMobile ? 18 : 24} color='black' onToggle={showSidebar}/>
             </div>
-            {/* <nav className={sidebar ? 'nav-bar active' : 'nav-bar'}>
-              <ul className='nav-menu-items'>
-              </ul>
-            </nav> */}
           </div>
       </div>
 
@@ -42,6 +44,18 @@ export default function Navbar({ isMobile }) {
           <a className='glow-on-hover' href="#">Schedule</a>
         </div>
       </div>
+
+      <nav className={sidebar ? 'nav-bar active animate__animated animate__slideInUp animate__faster' : 'nav-bar animate__animated animate__slideOutDown animate__faster'}>
+        <ul className='nav-menu-items'>
+          <li className='active'>
+            Home
+          </li>
+          <li>Events</li>
+          <li>Speakers</li>
+          <li>Schedule</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
 
       </>
     )
