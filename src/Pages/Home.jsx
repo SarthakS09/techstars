2b0140a666ef941ef461d9c409d5190c0fa1524c
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-03-15 19:03:08 +0530

    fixed netlify routing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -41,7 +41,7 @@ export default function Home({ isMobile }) {
           <p>24th MARCH 2023</p>
           <h3>E-Merge: The Official Pre-Event</h3>
           <div className="arrow">
-            <h3>REGISTER NOW</h3>
+            <h3>REGISTER NOW.</h3>
             <svg id="right">
               <path d="M0.5 9.35772H20.9956L14.2001 2.29941L16.4134 0L27 11L16.4134 22L14.2001 19.7006L20.9956 12.6423H0.5V9.35772Z" fill="#000"></path>
             </svg>
