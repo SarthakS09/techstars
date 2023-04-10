066e5c1d9093873c2550ecb492c38bcd85884218
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-04-11 02:50:03 +0530

    revamped home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,14 +3,17 @@ import { useEffect } from 'react'
 import { Link } from 'react-scroll'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import heroVideo from '../Assets/hero.mp4'
 import biggest from '../Assets/your-biggest.svg'
 import networking from '../Assets/networking.svg'
 import opportunity from '../Assets/opportunity.svg'
 import scrollDown from '../Assets/scroll.svg'
+import zeroToOne from '../Assets/zero-to-one.png'
+import pitchCafe from '../Assets/pitch-cafe.png'
+import startupFair from '../Assets/startup-fair.png'
 import sampleSpeakers from '../Assets/sample-speakers.png'
 import sampleCollabs from '../Assets/sample-collabs.png'
 import AOS from 'aos';
-import Spline from '@splinetool/react-spline';
 import 'aos/dist/aos.css';
 import '../Styles/Home.css'
 
@@ -23,25 +26,48 @@ export default function Home() {
     {/* <div className="spline">
     <Spline scene="https://prod.spline.design/vhZtkdWG2LvATHy7/scene.splinecode" />
     </div> */}
-    <div id="blob"></div>
-    <div id="blur"></div>
+    {/* <div id="blob"></div>
+    <div id="blur"></div> */}
+    <div className="hero">
+    <video className='hero-video' preload="auto" playsInline autoPlay loop muted>
+      <source src={heroVideo} type="video/mp4"/>
+    </video>
+    </div>
     <div className="landing">
+      <div className='landing-text'>
       <img className='animate__animated animate__fadeInUp t1' src={biggest}/>
       <img className='animate__animated animate__fadeInUp t2' src={networking}/>
       <img className='animate__animated animate__fadeInUp t3' src={opportunity}/>
+    </div>
       {/* <h1 className='t1 animate__animated animate__bounceInRight'>🚀 your biggest </h1>
       <h1 className='t2 animate__animated animate__bounceInLeft'>networking 👨🏻‍💻</h1>
       <h1 className='t3 animate__animated animate__bounceInRight'>💸 opportunity.</h1> */}
+      <div>
+      <h3 className='center date'>APRIL 23<sup>rd</sup> & 24<sup>th</sup>, 2023</h3>
       <Link style={{ width: "100%", display: 'flex', justifyContent: 'center' }} to="events-start" smooth={true} offset={-200} duration={1500}>
         <img src={scrollDown} className='animate__animated animate__bounceInUp animate__delay-2s scroller'/>
       </Link>
+      </div>
     </div>
 
     <div className='eventsSection'>
-      <h2 id='events-start' className='center' data-aos="fade-up">mind-blowing events. 🤯</h2>
+      <h2 id='events-start' className='center' data-aos="fade-up">Mind-Blowing Events. 🤯</h2>
       <p className='center' data-aos="fade-up">IIIT-Delhi's E-Summit'23 is packed with some of the most adrenaline-inducing events, but we'd like to give you a taste of what's to come before we steal the show.</p>
       <p className='center' data-aos="fade-up"><strong>Registrations are open for our offical pre-event</strong>, so what are you waiting for? Register now!</p>
-      <div className="two-events banner">
+      
+      <a href="https://forms.gle/spYit3unTSUq2o618">
+        <img src={startupFair} alt="Startup Fair" className="event-banner" />
+      </a>
+
+      <a href="https://forms.gle/TASM4aCHxcdSSE5RA">
+        <img src={zeroToOne} alt="Zero to One" className="event-banner" />
+      </a>
+      
+      <a href="https://forms.gle/buRwqAWz7uPKswzA7">
+        <img src={pitchCafe} alt="Pitch Cafe" className="event-banner" />
+      </a>  
+
+      {/* <div className="two-events banner">
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdVjZcApKfejIsxoGEc4MJPiuBkLW910rYK0F3gH88e644o8A/viewform">
         <div className="card-large" data-aos="fade-up">
           <p>24th MARCH 2023</p>
@@ -54,10 +80,10 @@ export default function Home() {
           </div>
         </div>
         </a>
-      </div>
+      </div> */}
       <br />
-      <h3>Here's what to expect:</h3>
-      <div className='two-events'>
+      {/* <h3>Here's what to expect:</h3> */}
+      {/* <div className='two-events'>
           <div className='card-large incubation' data-aos="fade-up">
             <img src='https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_400,h_467/https://iiitdic.in/wp-content/uploads/2020/03/66.jpg'/>
             <h3>Incubations' Meet</h3>
@@ -68,8 +94,8 @@ export default function Home() {
             <h3>Confluence: E-Cells' Meet</h3>
             <p>Confluence is an all-over Delhi-NCR's ECells meet, aiming to bring together the entrepreneurship cells from across the city on one platform and forum. This meeting aims to help different ECell groups to connect, collaborate and learn from each other. We plan to keep an open forum for all ECells to develop ideas and collaboration to pitch in front of all other ECell groups.</p>
           </div>
-      </div>
-      <div className="two-events">
+      </div> */}
+      {/* <div className="two-events">
           <div className="card-large scam-2023" data-aos="fade-up">
             <img src="https://images.indianexpress.com/2020/08/scam-1992-hanshad-mehta-teaser-1200.jpg?w=389" alt="scam-2023" />
             <h3>Scam 2023 (Mock Stock)</h3>
@@ -87,7 +113,7 @@ export default function Home() {
             <h3>Social Impact Meet</h3>
             <p>All hail Reformers! Book your calendars and join the movement of Change. We present name of event, an opportunity to interact and learn with fellow pacesetters. We intend to discuss themes like xyz, uvw….and hope to connect over the challenges and aspects we face as changemakers.</p>
           </div>
-      </div>
+      </div> */}
     </div>
 
     {/* <div className='speakers-card' data-aos="fade-up">
