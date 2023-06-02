9feb53102cd7af2cd0ebf7ef70ab1797879aa47a
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-06-02 21:52:17 +0530

    Removed Spline imports

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,6 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './App.css'
 
-function onDelete(id) {
-
-}
-
-function isPresent(id) {
-
-}
 
 function App() {
   // Initialising the Animate on Scroll (AOS) library
@@ -26,36 +19,44 @@ function App() {
     AOS.init();
   }, [])
 
-  // Initialising an object of preference mappings
-  const [bookmarks, setBookmarks] = useState({
-    'ashneerGrover': false,
-    'nishantSuri': false,
-    'triggeredInsaan': false,
-    'saptarishi': false,
-    'anshMehra': false,
-    'zeroToOne': false,
-    'startupFair': false,
-    'pitchCafe': false,
-    'raaz': false,
-    'adMad': false,
-    'sharkTank': false,
-    'godfather': false,
-    'socialIdeathon': false
-  });
+  // Fetching previous preferences from local storage.
+  // If no preference history is found, all bookmarks are assumed to be 'false'.
+  let temp;
+  if (localStorage.getItem("bookmarks"))
+    temp = JSON.parse(localStorage.getItem("bookmarks"));
+  else
+    temp = {
+      'ashneerGrover': false,
+      'nishantSuri': false,
+      'triggeredInsaan': false,
+      'saptarishi': false,
+      'anshMehra': false,
+      'zeroToOne': false,
+      'startupFair': false,
+      'pitchCafe': false,
+      'raaz': false,
+      'adMad': false,
+      'sharkTank': false,
+      'godfather': false,
+      'socialIdeathon': false
+    };
 
-  // if (localStorage.getItem("bookmarks"))
-  //   setBookmarks(JSON.parse(localStorage.getItem("bookmarks")));
+  // Creating a new object called 'bookmarks'. This is the object that will be used and
+  // passed around to keep track of user preferences in real time. In case of any changes
+  // to 'bookmarks' (such as the user adding or removing bookmarks to one or more events),
+  // the instance present in local storage will also update to mirror the change.
+  const [bookmarks, setBookmarks] = useState(temp);
+  useEffect(() => {
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+  }, [bookmarks])
 
+  // Helper functions to toggle the current bookmark state for a given 'id'
+  // and a function to simply return the state for a given 'id'.
   const toggleBookmark = (id) => {
-    console.log('toggle called!')
-    console.log(id)
-    console.log(bookmarks[id])
     setBookmarks((prevBookmarks) => ({
       ...prevBookmarks, [id]: !prevBookmarks[id],
     }));
-    console.log(bookmarks[id])
   }
-
   const hasOpted = (id) => {
     return bookmarks[id];
   }
@@ -67,7 +68,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/events" element={<Events toggleBookmark={toggleBookmark} hasOpted={hasOpted} />}/>
         <Route path="/speakers" element={<Speakers toggleBookmark={toggleBookmark} hasOpted={hasOpted} />}/>
-        <Route path="/schedule" element={<Schedule/>}/>
+        <Route path="/schedule" element={<Schedule toggleBookmark={toggleBookmark} hasOpted={hasOpted}/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path='*' element={<PageNotFound/>}/>
       </Routes>
--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -3,13 +3,13 @@ import { useEffect } from 'react'
 import schedImage from '../Assets/schedule.svg'
 import '../Styles/Schedule.scss'
 
-function renderBlock(time, lhc, rnd) {
+function renderBlock(time, lhc, rnd, lhcId, rndId, hasOpted, toggleBookmark) {
   return(
       <div className="time-section" data-aos="fade-up">
         <p className="sched-time">{time}</p>
         <div className="time-flex">
-          <div className={typeof rnd === 'undefined' ? 'time-card-empty' : 'time-card-rnd'}><h4>{rnd}</h4></div>
-          <div className="time-card-lhc"><h4>{lhc}</h4></div>
+          <button onClick={() => toggleBookmark(rndId)} className={`${rnd === '' ? 'time-card-empty' : 'time-card-rnd'} ${(hasOpted(rndId) && rnd !== '') ? 'time-card-imp' : ''}`} ><h4>{rnd}</h4></button>
+          <button onClick={() => toggleBookmark(lhcId)} className={hasOpted(lhcId) ? 'time-card-rnd time-card-imp' : 'time-card-rnd'}><h4>{lhc}</h4></button>
         </div>
       </div>
   )
@@ -26,15 +26,29 @@ function renderBreak(time, text) {
   )
 }
 
-export default function Schedule() {
+export default function Schedule(props) {
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
+
+  const hasOpted = (id) => {
+		return props.hasOpted(id)
+	}
+
+  const toggleBookmark = (id) => {
+    if (id === '') {
+      return
+    }
+		props.toggleBookmark(id);
+	}
+
   return (
     <>
       <div className="about-header sched-header">
         <h2>here's the plan.</h2>
         <p className='sched-p'>We recommend planning your day in advance to make the most of all the exciting things we have in store for you.</p>
+        <p className='sched-p'>If you have bookmarked any events/speaker talks on this website, they will be <strong>highlighted in white</strong> below. You may tap on the event cells below to bookmark them as well.</p>
       </div>
       <div className="sched-container">
         <div className="buildings" data-aos="fade-up">
@@ -42,18 +56,50 @@ export default function Schedule() {
           <span className="line"></span>
           <span><h4>LHC building</h4></span>
         </div>
-        {renderBlock('12:30pm - 1:30pm', 'registrations')}
-        {renderBlock('1:30pm - 2:30pm', 'enlightening session')}
-        {renderBlock('2:30pm - 3:15pm', 'Scam 2023: Round 1', 'Change Utsav: movie showcase/nukad natak')}
+        {renderBlock(
+          '12:30pm - 1:30pm',
+          'registrations',
+          '',
+          '', '', props.hasOpted, props.toggleBookmark)}
+        {renderBlock(
+          '1:30pm - 2:30pm',
+          'Keynote: Ansh Mehra',
+          'Zero to One',
+          'anshMehra', 'zeroToOne', props.hasOpted, props.toggleBookmark)}
+        {renderBlock(
+          '2:30pm - 3:15pm',
+          'Keynote: Saptarshi Prakash',
+          'Pitch Café',
+          'saptarishi', 'pitchCafe', props.hasOpted, props.toggleBookmark)}
 
-        {renderBreak('3:15pm - 3:45pm', 'snacks and networking')}
+        {renderBreak(
+          '3:15pm - 3:45pm',
+          'snacks and networking')}
 
-        {renderBlock('3:45pm - 5:15pm', 'Scam 2023: Round 2', 'Change Utsav')}
-        {renderBlock('5:15pm - 5:45pm', 'keynote session 1', 'Change Utsav')}
-        {renderBlock('5:45pm - 6:15pm', 'keynote session 2')}
-        {renderBlock('6:15pm - 7:15pm', 'confluence', 'empathy board & number painting')}
+        {renderBlock(
+          '3:45pm - 5:15pm',
+          'Performance: Nishant Suri', 
+          'Startup Fair', 
+          'nishantSuri', 'startupFair', props.hasOpted, props.toggleBookmark)}
+        {renderBlock(
+          '5:15pm - 5:45pm',
+          'Talk: Ashneer Grover',
+          'Ad Mad',
+          'ashneerGrover', 'adMad', props.hasOpted, props.toggleBookmark)}
+        {renderBlock(
+          '5:45pm - 6:15pm',
+          'Talk: Triggered Insaan',
+          'Shark Tank Panel',
+          'triggeredInsaan', 'sharkTank', props.hasOpted, props.toggleBookmark)}
+        {renderBlock(
+          '6:15pm - 7:15pm',
+          'Raaz',
+          'Godfather', 
+          'raaz', 'godfather', props.hasOpted, props.toggleBookmark)}
 
-        {renderBreak('7:15pm - 8:00pm', 'meal and open networking')}
+        {renderBreak(
+          '7:15pm - 8:00pm',
+          'meal and open networking')}
 
         {/* <img src={schedImage} alt="schedule" className='sched-image' /> */}
         
