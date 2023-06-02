a18faa33c3b9a6ba85a7b258a3cc24acb9df6455
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-06-02 22:00:36 +0530

    Tweaked text

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,16 +2,13 @@ import React, { useState } from 'react'
 import '../Styles/Card.scss'
 
 export default function Card(props) {
-	const [current, setCurrent]  = useState(false);
-
 	const toggleBookmark = (id) => {
 		props.toggleBookmark(id)
 	}
-
 	const hasOpted = (id) => {
 		return props.hasOpted(id)
 	}
-	
+
 	return (
 	<>
 		<div>
@@ -19,11 +16,9 @@ export default function Card(props) {
 				<img
 					src={props.img}
 					alt={props.id}
-					className="event-banner-min"
-				/>
+					className="event-banner-min" />
 			</a>
 			<button className='interest-bar' onClick={() => toggleBookmark(props.id)}>
-				{/* hello */}
 				{hasOpted(props.id) ? 'Bookmarked ✅' : 'Click to bookmark 📍'}
 			</button>
 		</div>
